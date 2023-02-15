29e885303daddc9c0116e49f7b77146b297645e2
Ameera Noormohamed <ameera.noormohamed@divurgent.com>
2023-02-15 15:35:40 -0800

    fixed some server.js errors and fixed file structure

--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,28 @@
-INSERT INTO employee (first_name, last_name, job_title, manager_id)
-VALUES ("Gary", "Smith", "Sales Representative", 5),
-       ("Lina", "Tao", "Senior Engineer", 4),
-       ("Jerry", "Springer", "Accountant", 3),
-       ("Hannah", "Meng", "Recruiter", 2);
+INSERT INTO department (name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (001, "Sales Representative", 60000, 1),
-       (003, "Senior Engineer", 180000, 2),
-       (004, "Accountant", 120000, 3),
-       (005, "Recruiter", 75000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Sales Lead", 105000, 1),
+        ("Salesperson", 90000, 1),
+        ("Lead Engineer", 160000, 2),
+        ("Software Engineer", 110000, 2),
+        ("Account Manager", 165000, 3),
+        ("Accountant", 133000, 3),
+        ("Legal Team Lead", 240000, 4),
+        ("Lawyer", 185000, 4);
 
-INSERT INTO department (id, department_name)
-VALUES (001, "Sales"),
-       (002, "Engineering"),
-       (003, "Finance"),
-       (004, "Human Resources");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("April", "Sanchez", 1, null),
+        ("Frank", "Goodwin", 2, 1),
+        ("Matt", "James", 3, null),
+        ("Hannah", "Smith", 4, 3),
+        ("Logan", "Brown", 5, null),
+        ("Lee", "Butler", 6, 5),
+        ("Nicole", "Lourd", 7, null),
+        ("Jerome", "Allen", 8, 7);
+
+
+
